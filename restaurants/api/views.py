7e066d1d8cfc7f0e6795245364dc1f819a51246c
InaraShalfei <inarashugaikina@gmail.com>
2022-02-14 14:18:26 +0500

    Create viewsets

--- restaurants/api/views.py
+++ restaurants/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from .models import Pizza, Restaurant
+from .serializers import PizzaSerializer, RestaurantSerializer
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name', 'address')
+
+
+class PizzaViewSet(viewsets.ModelViewSet):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('name', 'restaurant')
+    search_fields = ('^name', '^cheese', '^pastry')
