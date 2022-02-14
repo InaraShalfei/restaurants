e78d0876c7a42d8aa0197878b0bfedb57cf9bd1e
InaraShalfei <inarashugaikina@gmail.com>
2022-02-14 14:15:33 +0500

    Register models in admin zone

--- restaurants/api/admin.py
+++ restaurants/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Restaurant, Pizza
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'restaurant', 'secret_ingredient')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Pizza, PizzaAdmin)
