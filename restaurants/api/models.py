1d4260048f0a04cbc6fab8ecce62faace0c9a739
InaraShalfei <inarashugaikina@gmail.com>
2022-02-14 14:04:40 +0500

    Create models and make migrations

--- restaurants/api/models.py
+++ restaurants/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    address = models.CharField(max_length=128, blank=False)
+
+    class Meta:
+        verbose_name = 'restaurant'
+        verbose_name_plural = 'restaurants'
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    cheese = models.CharField(max_length=128, blank=True)
+    pastry = models.CharField(max_length=128, blank=True)
+    secret_ingredient = models.CharField(max_length=128, blank=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE,
+                                   related_name='pizzas')
+
+    class Meta:
+        verbose_name = 'pizza'
+        verbose_name_plural = 'pizzas'
+
+    def __str__(self):
+        return f'{self.name} с секретным ингредиентом {self.secret_ingredient}'
--- restaurants/restaurants/settings.py
+++ restaurants/restaurants/settings.py
@@ -19,7 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'rest_framework',
+    'django_filters',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -90,3 +92,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        ],
+    'DEFAULT_FILTER_BACKENDS': [
+         'django_filters.rest_framework.DjangoFilterBackend'
+                                   ],
+}
