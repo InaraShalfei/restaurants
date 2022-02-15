ffd1fe313d7c5a1c187a16832bb116bca22e35e7
InaraShalfei <inarashugaikina@gmail.com>
2022-02-15 18:45:01 +0500

    Add allowed hosts into settings

--- restaurants/restaurants/settings.py
+++ restaurants/restaurants/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '85.143.172.31', '127.0.0.1', 'web']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
